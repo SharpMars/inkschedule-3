f00c8247be89883107aebbe0833f53bdc6747abd
SharpMars <70289680+SharpMars@users.noreply.github.com>
2023-10-13 10:04:04 +0200

    Fix error in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import solidPlugin from "vite-plugin-solid";
 import unocssPlugin from "unocss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
+export default defineConfig({
   plugins: [unocssPlugin(), solidPlugin()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
@@ -25,4 +25,4 @@ export default defineConfig(async () => ({
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG
   }
-}));
+});
