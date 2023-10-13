64b8b28d838574ce5b8f600cc174018a61f365f0
SharpMars <70289680+SharpMars@users.noreply.github.com>
2023-10-13 10:49:40 +0200

    Error screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense, createResource, createSignal } from "solid-js";
+import { ErrorBoundary, Suspense, createResource, createSignal } from "solid-js";
 import { Navbar, Tab } from "./Navbar";
 import { getCurrentSchedule } from "./schedule";
 import { EntryList } from "./entries/EntryList";
@@ -24,12 +24,25 @@ function App() {
   }, 3000);
 
   return (
-    <>
+    <ErrorBoundary
+      fallback={(err) => (
+        <div class="flex flex-col items-center overflow-y-auto overflow-x-hidden h-600px">
+          <h2>Error has occured</h2>
+          <p class="p-2 bg-dark-1 rounded m-l-2 m-r-2 font-mono text-sm">{err.toString()}</p>
+          <button
+            class="p-3 font-bold text-size-4 color-white bg-neutral-500 b-0 rounded hover:filter-brightness-90% active:filter-brightness-70% transition-filter"
+            onclick={() => location.reload()}
+          >
+            Refresh
+          </button>
+        </div>
+      )}
+    >
       <Navbar setTab={setTab}></Navbar>
       <Suspense fallback={<p class="text-center font-bold">Loading...</p>}>
         <EntryList getTab={getTab} getSchedule={getSchedule}></EntryList>
       </Suspense>
-    </>
+    </ErrorBoundary>
   );
 }
 
