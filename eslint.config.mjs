17a13a9bdd9db1f956bf61aceb2f6eb99a5cbe7a
SharpMars <70289680+SharpMars@users.noreply.github.com>
2024-06-22 23:15:06 +0200

    Add ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import solid from "eslint-plugin-solid/configs/typescript.js";
+import * as tsParser from "@typescript-eslint/parser";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    ...solid,
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "tsconfig.json",
+      },
+    },
+  },
+  {
+    ignores: ["dist/", "src-tauri/", "node_modules/", ".vscode/"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "prefer-const": "warn"
+    }
+  }
+];
