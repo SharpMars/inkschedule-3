7f66e0f5e5ff7ff12ba10c67afc2837bc249c949
SharpMars <70289680+SharpMars@users.noreply.github.com>
2024-09-21 15:43:41 +0200

    Move ErrorBoundary to top most component Fixes #7 and #8

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ErrorBoundary, Suspense, createEffect, createResource, createSignal, on } from "solid-js";
+import { Suspense, createEffect, createResource, createSignal, on } from "solid-js";
 import { Navbar, Tab } from "./Navbar";
 import { getCurrentSchedule } from "./schedule";
 import { EntryList } from "./entries/EntryList";
@@ -51,25 +51,12 @@ function App() {
   );
 
   return (
-    <ErrorBoundary
-      fallback={(err) => (
-        <div class="flex flex-col items-center overflow-y-auto overflow-x-hidden h-600px">
-          <h2>Error has occured</h2>
-          <p class="p-2 bg-dark-1 rounded m-l-2 m-r-2 font-mono text-sm">{err.toString()}</p>
-          <button
-            class="p-3 font-bold text-size-4 color-white bg-neutral-500 b-0 rounded hover:filter-brightness-90% active:filter-brightness-70% transition-filter"
-            onClick={() => location.reload()}
-          >
-            Refresh
-          </button>
-        </div>
-      )}
-    >
+    <>
       <Navbar setTab={setTab} getSchedule={getSchedule} />
       <Suspense fallback={<p class="text-center font-bold h-[calc(100vh-46px)]">Loading...</p>}>
         <EntryList getTab={getTab} getSchedule={getSchedule} />
       </Suspense>
-    </ErrorBoundary>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 /* @refresh reload */
-import { render } from "solid-js/web";
+import { ErrorBoundary, render } from "solid-js/web";
 
 import "virtual:uno.css";
 import "./styles.css";
@@ -23,4 +23,25 @@ listen("set_tab", (event) => {
 });
 
 document.addEventListener("contextmenu", (event) => event.preventDefault());
-render(() => <App />, document.getElementById("root") as HTMLElement);
+render(() => <WrappedApp />, document.getElementById("root") as HTMLElement);
+
+function WrappedApp() {
+  return (
+    <ErrorBoundary
+      fallback={(err) => (
+        <div class="flex flex-col items-center overflow-y-auto overflow-x-hidden h-600px">
+          <h2>Error has occured</h2>
+          <p class="p-2 bg-dark-1 rounded m-l-2 m-r-2 font-mono text-sm">{err.toString()}</p>
+          <button
+            class="p-3 font-bold text-size-4 color-white bg-neutral-500 b-0 rounded hover:filter-brightness-90% active:filter-brightness-70% transition-filter"
+            onClick={() => location.reload()}
+          >
+            Refresh
+          </button>
+        </div>
+      )}
+    >
+      <App />
+    </ErrorBoundary>
+  );
+}
